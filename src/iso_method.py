325ab4948a5e4828c0d5fb1d6ba41eacfa089bc9
Daniil153 <50330853+Daniil153@users.noreply.github.com>
2021-06-18 14:58:04 +0300

    Update iso_method.py

--- src/iso_method.py
+++ src/iso_method.py
@@ -26,18 +26,17 @@ def fmean(df_temp):
     qq.sort_index(inplace=True)
     return list(qq.index), list(qq.scores)
 
-def acc1(x, y):
-    return accuracy_score(x, y)
 
 def run_model(train_gold=[(path_to_data + 'train_1.rusemshift.tsv', path_to_gold + 'train_1.rusemshift.gold.tsv'),
                           (path_to_data + 'train_2.rusemshift.tsv', path_to_gold + 'train_2.rusemshift.gold.tsv')],
               dev=[path_to_data + 'dev_1.rusemshift.tsv', path_to_data + 'dev_2.rusemshift.tsv',
-                   path_to_data + 'dev_1.scd_12_sl-True_fl-True_np-100.tsv'],
+                   path_to_data + 'dev_1.scd_12_sl-False_fl-False_np-100.tsv'],
               dev_gold=[(path_to_data + 'dev_1.rusemshift.tsv', path_to_gold + 'dev_1.rusemshift.gold.tsv'),
                         (path_to_data + 'dev_2.rusemshift.tsv', path_to_gold + 'dev_2.rusemshift.gold.tsv')],
-              test=[path_to_data + 'test.scd_12_sl-True_fl-True_np-100.tsv',
-                    path_to_data + 'test.scd_23_sl-True_fl-True_np-100.tsv',
-                    path_to_data + 'test.scd_13_sl-True_fl-True_np-100.tsv'],	
+              test=[path_to_data + 'test.scd_12_sl-False_fl-False_np-100.tsv',
+                    path_to_data + 'test.scd_23_sl-False_fl-False_np-100.tsv',
+                    path_to_data + 'test.scd_13_sl-False_fl-False_np-100.tsv'],
+              test_gold='rushiftEval/eval_answer.tsv',
               model_name=''):
     """
     launches a model that calculates metrics using isotonic regression on the scores for sentences and saves the metrics
@@ -49,7 +48,11 @@ def run_model(train_gold=[(path_to_data + 'train_1.rusemshift.tsv', path_to_gold
     :param model_name: wic-model name
     :return:
     """
-    X_train, y_train = [], []
+    X_train, y_train, x_train_w, y_train_w = [], [], [], []
+    df_dev = pd.DataFrame()
+    t = [a[0].split('/')[-1] for a in train_gold]
+    t = "+".join(t)
+    df_dev['model'] = [f'iso_{model_name}_trained-{t}']
     for j in train_gold:
         df_temp_ans = pd.read_csv(j[1], sep='\t')
         df_temp_ans_data = pd.read_csv(j[0], sep='\t')
@@ -57,17 +60,13 @@ def run_model(train_gold=[(path_to_data + 'train_1.rusemshift.tsv', path_to_gold
         df_temp_ans_data = df_temp_ans_data.merge(df_temp_ans, on='id', how='inner')
         X_train.extend(list(df_temp_ans_data.scores))
         y_train.extend(list(df_temp_ans_data.gold_score))
-    df_dev = pd.DataFrame()
-    t = [a[0].split('/')[-1] for a in train_gold]
-    t = "+".join(t)
-    df_dev['model'] = [f'iso_{model_name}_trained-{t}']
     devs_gold, dfs_dev_ans = {}, {}
     for i in dev_gold:
         df_temp_ans = pd.read_csv(i[1], sep='\t')
         df_temp_ans_data = pd.read_csv(i[0], sep='\t')
         df_temp_ans = df_temp_ans.rename(columns={'score': 'gold_score', 'tag': 'gold_tag'})
         df_temp_ans_data = df_temp_ans_data.merge(df_temp_ans, on='id', how='inner')
-        if '_1.' in i[0]:
+        if '_1.' in i[0].split('/')[-1]:
             temp = 'dev_1'
         else:
             temp = 'dev_2'
@@ -89,12 +88,13 @@ def run_model(train_gold=[(path_to_data + 'train_1.rusemshift.tsv', path_to_gold
             else:
                 df_temp_ans_data = dfs_dev_ans['dev_2']
             sent_spearman = spearman(df_temp_ans_data.gold_score, X_dev)
-            acc = acc1(list(df_temp_ans_data.tag), list(df_temp_ans_data.gold_tag))
+            acc = accuracy_score(list(df_temp_ans_data.tag), list(df_temp_ans_data.gold_tag))
             df_dev[i.split('/')[-1] + '_accuracy'] = [acc]
             df_dev[i.split('/')[-1] + '_sent_spearman'] = [sent_spearman]
         df_dev[i.split('/')[-1] + '_word_spearman'] = [corr]
-    df_dev.to_csv(f'{model_name}_trained-{t}_iso_dev_metrics.tsv', sep='\t', index=False, header=True)
+    ppath = model_name.replace('+', '/')
     df_subm = pd.DataFrame()
+    dfs_test_ans = {}
     for i in test:
         df_temp = pd.read_csv(i, sep='\t')
         X_test = df_temp.scores
@@ -106,8 +106,22 @@ def run_model(train_gold=[(path_to_data + 'train_1.rusemshift.tsv', path_to_gold
         if len(df_subm) == 0:
             df_subm['word'] = words
         df_subm[i.split('/')[-1]] = scores
+    if test_gold:
+        columns_subm = list(df_subm.columns)
+        df_subm = df_subm.rename(
+            columns={columns_subm[1]: '12_pred', columns_subm[2]: '23_pred', columns_subm[3]: '13_pred'})
+        df_an = pd.read_csv(test_gold, sep='\t', names=['word', '12_gold', '23_gold', '13_gold'])
+        df_an = df_an.merge(df_subm, on='word', how='inner')
+        ans12 = spearman(df_an['12_pred'], df_an['12_gold'])
+        ans23 = spearman(df_an['23_pred'], df_an['23_gold'])
+        ans13 = spearman(df_an['13_pred'], df_an['13_gold'])
+        df_dev[f'sampled_test12_w_spearman'] = [ans12]
+        df_dev[f'sampled_test13_w_spearman'] = [ans13]
+        df_dev[f'sampled_test23_w_spearman'] = [ans23]
+        df_dev[f'avg_test_w_spearman'] = [(ans13 + ans12 + ans23) / 3]
     temp = df_subm.columns[1]
-    df_subm.to_csv(f'{model_name}_trained-{t}_iso_answer_{temp.split(".")[1]}.tsv', sep='\t', index=False, header=False)
+    df_dev.to_csv(f'iso_dev_metrics.tsv', sep='\t', index=False, header=True)
+    df_subm.to_csv(f'iso_answer.tsv', sep='\t', index=False, header=False)
 
 if __name__ == '__main__':
     fire.Fire(run_model)
