818bb6202548a060dd84fee1175d07ac510adc31
Daniil153 <50330853+Daniil153@users.noreply.github.com>
2021-06-18 20:52:45 +0300

    Update eval_best_eval_model.sh

--- eval_best_eval_model.sh
+++ eval_best_eval_model.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-ckpt_path=$(dirname $1)
 mkdir for_gold
 cp -r rushiftEval/*rusemshift for_gold/
 python src/tsv_to_json_gold.py --path_to_combine for_gold/
 python src/sample_scr.py --path_to_test rushiftEval/Evaluation_words/ --path_to_dev rushiftEval/RuSemShift_instances/ --path_to_sampled pairs/ --path_to_combine pairs/combine/ --in_words_dev1 rushiftEval/testset1.tsv --in_words_dev2 rushiftEval/testset2.tsv --delete_short_and_long True --delete_first_and_last_position True --n_pairs 100
 echo "sample comlete"
 cd mcl-wic
-bash run_wic_model.sh ckpt_path ${@:2}
+!python run_model.py --do_eval --ckpt_path ../first_concat --eval_input_dir ../pairs/combine/sampled_data --eval_output_dir rusemshift_predictions/ --output_dir ../first_concat --loss mse_loss --pool_type first --symmetric true train_scd 
 mkdir sampled_data
 mkdir sampled_data/score
 mkdir sampled_data/ans
