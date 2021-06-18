56cbb8bdba4f5e04ab555e5a9ca1456edd2d45f5
Daniil153 <50330853+Daniil153@users.noreply.github.com>
2021-06-18 19:50:37 +0300

    Update run_wic_model.sh

--- run_wic_model.sh
+++ run_wic_model.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
 #SBATCH --gres=gpu:1
 ckpt_path=$(dirname $1)
-python run_model.py --do_eval --ckpt_path $ckpt_path --eval_input_dir pairs/combine/sampled_data --eval_output_dir rusemshift_predictions/ --output_dir preds-rusemshift ${@:2}
-
+python run_model.py --do_eval --ckpt_path $ckpt_path --eval_input_dir ../pairs/combine/sampled_data --eval_output_dir rusemshift_predictions/ ${@:2}
