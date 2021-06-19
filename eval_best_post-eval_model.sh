4e11da7d49b37b6fa3c9daa5be478249907d06dd
Daniil153 <50330853+Daniil153@users.noreply.github.com>
2021-06-19 19:06:48 +0300

    Update eval_best_post-eval_model.sh

--- eval_best_post-eval_model.sh
+++ eval_best_post-eval_model.sh
@@ -5,7 +5,7 @@ python src/tsv_to_json_gold.py --path_to_combine for_gold/
 python src/sample_scr.py --path_to_test rushiftEval/Evaluation_words/ --path_to_dev rushiftEval/RuSemShift_instances/ --path_to_sampled pairs/ --path_to_combine pairs/combine/ --in_words_dev1 rushiftEval/testset1.tsv --in_words_dev2 rushiftEval/testset2.tsv --delete_short_and_long True --delete_first_and_last_position True --n_pairs 100
 echo "sample comlete"
 cd mcl-wic
-python run_model.py --do_eval --ckpt_path ../mean_dist_l1ndotn_CE --eval_input_dir ../pairs/combine/sampled_data --eval_output_dir rusemshift_predictions/ --output_dir ../first_concat --loss crossentropy_loss --pool_type mean --symmetric true --train_scd 
+python run_model.py --do_eval --ckpt_path ../mean_dist_l1ndotn_CE --eval_input_dir ../pairs/combine/sampled_data --eval_output_dir rusemshift_predictions/ --output_dir ../mean_dist_l1ndotn_CE --loss crossentropy_loss --pool_type mean --symmetric true --train_scd 
 cd ..
 mkdir sampled_data
 mkdir sampled_data/score
