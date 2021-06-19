16b2a8acecb9a237ccf5a59ec6eeb09ed3ae0ecb
Daniil153 <50330853+Daniil153@users.noreply.github.com>
2021-06-19 14:49:43 +0300

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -2,3 +2,9 @@
 wget https://zenodo.org/record/4981585/files/first_concat.zip
 unzip first_concat.zip
 rm first_concat.zip
+wget https://zenodo.org/record/4992633/files/mean_dist_l1ndotn_MSE.zip
+unzip mean_dist_l1ndotn_MSE.zip
+rm mean_dist_l1ndotn_MSE.zip
+wget https://zenodo.org/record/4992613/files/mean_dist_l1ndotn_CE.zip
+unzip mean_dist_l1ndotn_CE.zip
+rm mean_dist_l1ndotn_CE.zip
