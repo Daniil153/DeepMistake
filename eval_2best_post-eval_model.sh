920727ad64693f375c46e41f69fcd3209c046761
Daniil153 <50330853+Daniil153@users.noreply.github.com>
2021-06-24 10:34:41 +0300

    Update eval_2best_post-eval_model.sh

--- eval_2best_post-eval_model.sh
+++ eval_2best_post-eval_model.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir for_gold
+mkdir -p for_gold
 cp -r rushiftEval/*rusemshift for_gold/
 python src/tsv_to_json_gold.py --path_to_combine for_gold/
 python src/sample_scr.py --path_to_test rushiftEval/Evaluation_words/ --path_to_dev rushiftEval/RuSemShift_instances/ --path_to_sampled pairs/ --path_to_combine pairs/combine/ --in_words_dev1 rushiftEval/testset1.tsv --in_words_dev2 rushiftEval/testset2.tsv --delete_short_and_long True --delete_first_and_last_position True --n_pairs 100
@@ -7,9 +7,9 @@ echo "sample comlete"
 cd mcl-wic
 python run_model.py --do_eval --ckpt_path ../mean_dist_l1ndotn_MSE --eval_input_dir ../pairs/combine/sampled_data --eval_output_dir rusemshift_predictions/ --output_dir ../mean_dist_l1ndotn_MSE --loss mse_loss --pool_type mean --symmetric true --train_scd 
 cd ..
-mkdir sampled_data
-mkdir sampled_data/score
-mkdir sampled_data/ans
+mkdir -p sampled_data
+mkdir -p sampled_data/score
+mkdir -p sampled_data/ans
 mv mean_dist_l1ndotn_MSE/rusemshift_predictions/*.scores sampled_data/score/
 mv mean_dist_l1ndotn_MSE/rusemshift_predictions/* sampled_data/ans/ 
 python src/constr.py --type mean --model_name mean_dist_l1ndotn_MSE
